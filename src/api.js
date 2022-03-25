0fe7f238a50d9e671385ed22400de74dbb9b2b6c
Junaid <zulfiqar.junaid95@gmail.com>
2022-03-25 01:50:21 -0700

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,36 @@
+export const questions = [
+
+    {
+        id: 1,
+        question: 'How do you create a React app?',
+        answer: 'Install the create-react-app package using the command prompt or terminal.'
+    },
+    {
+        id: 2,
+        question: 'What is React Router?',
+        answer: 'React Router is a routing library built on top of React, which is used to create routes in a React application. '
+    },
+    {
+        id: 3,
+        question: 'What are hooks in react js?',
+        answer: "Hooks are the new feature introduced in the React 16.8 version. It allows you to use state and other React features without writing a class. Hooks are the functions which hook into React state and lifecycle features from function components. It does not work inside classes."
+    },
+
+    {
+        id: 4,
+        question: 'What are the advantages of react?',
+        answer: 'SOME ADDITIONAL ADVANTAGES OF REACTJ: Makes JavaScript coding easier,Extremely competent,Excellent cross-platform support,Handles dependencies,Template designing made easy,Provides amazing developer tools,UI focused designs,Easy to adopt.'
+    },
+
+    {
+        id: 5,
+        question: 'What is an event in React?',
+        answer: 'An event is an action that a user or system may trigger, such as pressing a key, a mouse click, etc.'
+    },
+    {
+        id: 6,
+        question: 'Why use functional components in React?',
+        answer: 'Functional components are some of the more common components that will come across while working in React. These are simply JavaScript functions. We can create a functional component to React by writing a JavaScript function. These functions may or may not receive data as parameters.'
+    }
+
+]
